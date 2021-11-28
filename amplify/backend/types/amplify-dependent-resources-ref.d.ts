e8ac3edcaae2be16eee19ba04bb3e11a0d333536
Jhonatan Leme <jw_leme@hotmail.com>
2021-11-28 15:38:51 -0300

    adicao banco de dados

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,12 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "manutencaoapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
     }
 }
--- src/App.js
+++ src/App.js
@@ -1,27 +1,81 @@
 /*************************
  Arquivo src/App.js 
 *************************/
-import React from 'react';
+
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
-import { Amplify } from 'aws-amplify';
-
+import { API } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import { listTodos } from './graphql/queries';
+import { createTodo as createNoteMutation, deleteTodo as deleteNoteMutation } from './graphql/mutations';
 
+import { Amplify } from 'aws-amplify';
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
-function App({ isPassedToWithAuthenticator, signOut, user }) {
+const initialFormState = { name: '', description: '' }
+
+ function App({ isPassedToWithAuthenticator, signOut, user }) {
+  const [Todo, setTodo] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
   if (!isPassedToWithAuthenticator) {
     throw new Error(`isPassedToWithAuthenticator was not provided`);
   }
 
+  useEffect(() => {
+    fetchTodo();
+  }, []);
+
+  async function fetchTodo() {
+    const apiData = await API.graphql({ query: listTodos });
+    setTodo(apiData.data.listTodos.items);
+  }
+
+  async function createNote() {
+    if (!formData.name || !formData.description) return;
+    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+    setTodo([ ...Todo, formData ]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteNote({ id }) {
+    const newTodoArray = Todo.filter(note => note.id !== id);
+    setTodo(newTodoArray);
+    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+  }
+
   return (
-    <>
-      <h1>Hello {user.username}</h1>
-      <button onClick={signOut}>Sign out</button>
-    </>
+    <div className="App">
+      <h1>My Todo App</h1>
+      <input
+        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+        placeholder="Note name"
+        value={formData.name}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+        placeholder="Note description"
+        value={formData.description}
+      />
+      <button onClick={createNote}>Create Note</button>
+      <div style={{marginBottom: 30}}>
+        {
+          Todo.map(note => (
+            <div key={note.id || note.name}>
+              <h2>{note.name}</h2>
+              <p>{note.description}</p>
+              <button onClick={() => deleteNote(note)}>Delete note</button>
+            </div>
+          ))
+        }
+      <h1>Olá {user.username}</h1>
+      <button onClick={signOut}>Sair</button>
+      </div>
+      <withAuthenticator />
+    </div>
+    
   );
 }
 
