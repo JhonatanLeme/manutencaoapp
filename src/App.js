d78aee63eedb15c07caf55103796878c5e093dfe
Jhonatan Leme <jw_leme@hotmail.com>
2021-11-28 11:41:36 -0300

    Inital Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,3 @@
-************************
-/* Arquivo src/App.js */
-************************
-
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -11,7 +6,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Seja bem vindo Jhonatan Leme</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
